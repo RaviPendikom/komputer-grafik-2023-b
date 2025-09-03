4c0f7a5795ebc0e0b780a4950fd5b7e1d16e2536
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-09-03 09:22:55 +0800

    Add Animation bounce ball radius - Done

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 let x = 300;
 let y = 300;
 let radius = 100;
-let speedX = 2;
+let speedX = 8;
 let speedY = 5;
 
 function setup() {
   createCanvas(600, 600);
+  background(145, 89, 78);
 }
 
 function draw() {
   noStroke();
-  background(145, 89, 78);
-  fill(156, 200, 78);
-  circle(x, y, radius);
+  fill(random(255), random(255), random(255), random(255));
+  circle(x, y, random(100));
   if (x + 50 >= 600 || x - 50 <= 0) {
     speedX *= -1;
   }
