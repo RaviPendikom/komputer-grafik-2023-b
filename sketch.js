0e8ebb435ff9fd7cd91a441efe025a7d99bb0a83
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-09-03 08:45:36 +0800

    Draw Function - Done

--- sketch.js
+++ sketch.js
@@ -4,9 +4,11 @@ let radius = 150;
 
 function setup() {
   createCanvas(600, 600);
+}
+
+function draw() {
   background(145, 89, 78);
   noStroke();
-  fill(10, 20, 30);
+  fill(46, 198, 89);
   circle(x, y, radius);
-  square(x, y, radius);
 }
