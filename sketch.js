3a21052ba965b2b078a2cc3a5d3aab3b9d1a1e81
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-09-03 08:40:24 +0800

    Variable - Done

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
+let x = 300;
+let y = 300;
+let radius = 150;
+
 function setup() {
   createCanvas(600, 600);
   background(145, 89, 78);
   noStroke();
   fill(10, 20, 30);
-  circle(300, 300, 150);
-  square(300, 300, 150);
+  circle(x, y, radius);
+  square(x, y, radius);
 }
